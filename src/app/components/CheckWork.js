d3961191469573b75ed696e55202a93da29b4b96
developersushan <sushansing709sushansing@gmail.com>
2024-07-31 05:23:00 +0600

    design done

--- src/app/components/CheckWork.js
+++ src/app/components/CheckWork.js
@@ -1,3 +1,4 @@
+"use client"
 import React from 'react'
 import Slider from '../hooks/Slider'
 import LeftArrowBtn from '../hooks/LeftArrowBtn'
--- src/app/hooks/RightArrowBtn.js
+++ src/app/hooks/RightArrowBtn.js
@@ -2,9 +2,7 @@
 import Image from 'next/image'
 import React from 'react'
 import RightArrow from '../assets/Vector1.svg'
-import { useSwiper } from 'swiper/react'
 const RightArrowBtn = () => {
-    let swiper = useSwiper()
   return (
     <div className=''>
         <button >
